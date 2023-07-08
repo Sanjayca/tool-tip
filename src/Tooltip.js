58693beb10e3d1838c14d88b1f8a2131a3431942
Sanjay <sanjay1box@gmail.com>
2023-07-08 12:08:39 +0530

    format

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
 
+class Tooltip extends React.Component {
+  class = () => {
+    return `${this.props.position}`;
+  };
 
-
-class Tooltip extends React.Component{
-    class=()=>{
-        
-        return `${this.props.position}`;
-    }
-
-    render(){
-        return (
-            <div className={`${this.class()}`}>
-                Thanks for hovering! I'm a tooltip
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={`${this.class()}`}>
+        Thanks for hovering! I'm a tooltip
+      </div>
+    );
+  }
 }
-
 
 export default Tooltip;
